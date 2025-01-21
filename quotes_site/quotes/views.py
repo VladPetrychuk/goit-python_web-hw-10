05ef5bb4e09c19f01be9221fe0b4bb1520dc42e7
Vladyslav Petrychuk <tomek05120512@gmail.com>
2025-01-21 18:26:28 +0200

    Add making authors

--- quotes_site/quotes/views.py
+++ quotes_site/quotes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import RegistrationForm
+from django.contrib.auth.decorators import login_required
+from .models_mongo import Author
 
 def register(request):
     if request.method == 'POST':
@@ -12,3 +14,23 @@ def register(request):
     else:
         form = RegistrationForm()
     return render(request, 'quotes/register.html', {'form': form})
+
+@login_required
+def add_author(request):
+    if request.method == 'POST':  # Користувач надіслав дані
+        fullname = request.POST.get('fullname')
+        born_date = request.POST.get('born_date')
+        born_location = request.POST.get('born_location')
+        description = request.POST.get('description')
+
+        # Створюємо нового автора
+        author = Author(
+            fullname=fullname,
+            born_date=born_date,
+            born_location=born_location,
+            description=description
+        )
+        author.save()  # Зберігаємо в MongoDB
+        return redirect('quotes:author_list')  # Переходимо до списку авторів
+
+    return render(request, 'quotes/add_author.html')  # Виводимо форму
