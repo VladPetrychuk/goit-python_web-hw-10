6a0d9f977e985d9ca31d8c75c9eda313a3d6067c
Vladyslav Petrychuk <tomek05120512@gmail.com>
2025-01-21 18:38:18 +0200

    Update models

--- models.py
+++ models.py
@@ -1,18 +1,25 @@
 from mongoengine import Document, StringField, ReferenceField, ListField, BooleanField, EmailField, connect
+from django.db import models
 
 # Підключення до MongoDB
 connect(db="email_system", host="mongodb+srv://dbuser:2KocWCKPaVTi5vzI@cluster0.pmbcv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-class Author(Document):
-    fullname = StringField(required=True, unique=True)
-    born_date = StringField()
-    born_location = StringField()
-    description = StringField()
+class Author(models.Model):
+    fullname = models.CharField(max_length=255)
+    born_date = models.CharField(max_length=255, blank=True, null=True)
+    born_location = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
 
-class Quote(Document):
-    tags = ListField(StringField())
-    author = ReferenceField(Author, required=True)
-    quote = StringField(required=True)
+    def __str__(self):
+        return self.fullname
+
+class Quote(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='quotes')
+    tags = models.CharField(max_length=255, blank=True, null=True)  # Теги через кому
+
+    def __str__(self):
+        return self.text[:50]
 
 class Contact(Document):
     fullname = StringField(required=True)
