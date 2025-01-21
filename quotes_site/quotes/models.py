83dd5e5161c4acac114c0647f89d625e3519431d
Vladyslav Petrychuk <tomek05120512@gmail.com>
2025-01-21 18:30:32 +0200

    Creatie models for PostgreSQL

--- quotes_site/quotes/models.py
+++ quotes_site/quotes/models.py
@@ -3,3 +3,20 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     pass
+
+class Author(models.Model):
+    fullname = models.CharField(max_length=255)
+    born_date = models.CharField(max_length=255, blank=True, null=True)
+    born_location = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.fullname
+
+class Quote(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='quotes')
+    tags = models.CharField(max_length=255, blank=True, null=True)  # Зберігаємо теги як строку через кому
+
+    def __str__(self):
+        return self.text[:50]
