d0e919b92609017b7d1ad7dd45703c8bb05ee406
Vladyslav Petrychuk <tomek05120512@gmail.com>
2025-01-21 18:17:17 +0200

    Update for registration

--- quotes_site/quotes/views.py
+++ quotes_site/quotes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('quotes:index')
+    else:
+        form = RegistrationForm()
+    return render(request, 'quotes/register.html', {'form': form})
